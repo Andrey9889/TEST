fa4680b2e673c1abf5c619e007f182e24aa050e2
Andrey <doguyoola@bk.ru>
2024-07-21 15:05:08 +0700

    first

---
+++ main.py
@@ -0,0 +1,23 @@
+x = int(input())
+y = int(input())
+print(x + y)
+
+
+x = int(input())
+y = int(input())
+c = int(input())
+print(x + y + c)
+
+
+x = int(input())
+y = int(input())
+print (x - y)
+
+
+x = int(input())
+y = int(input())
+print(x * y)
+
+x = int(input())
+y = int(input())
+print(x / y)
